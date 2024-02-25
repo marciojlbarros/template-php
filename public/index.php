eacef8b1e6cf230cebef802148e9787fef0e9ff9
Marcio Barros <marciojlbarros@gmail.com>
2024-02-24 21:40:34 -0300

    How Output Buffer

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Home';
+$data = ['name' => 'Marcio'];
+
+ob_start();
+
+extract($data);
+
+require 'home.php';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump('content', $content);
