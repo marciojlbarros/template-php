39ba95546770a695c31dc565bad80e21859d5980
Marcio Barros <marciojlbarros@gmail.com>
2024-02-26 21:53:06 -0300

    Database connection

---
+++ app/framework/database/Connection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\framework\database;
+
+class Connection
+{
+    private static $connection;
+
+    public static function getConnection()
+    {
+        if (empty(self::$connection)) {
+            self::$connection = new \PDO("mysql:host={$_ENV['DATABASE_HOST']};dbname={$_ENV['DATABASE_NAME']}", $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD'], [
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+            ]);
+        }
+
+        return self::$connection;
+    }
+}
